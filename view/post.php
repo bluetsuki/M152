4ac4386d9f6a93b7cc1e41eaf4031e2c491150ab
bluetsuki <kelly.ngn@eduge.ch>
2020-01-28 10:41:56 +0100

    design update

--- view/post.php
+++ view/post.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Post</title>
-  </head>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Post</title>
+</head>
 <body>
   <div class="container">
     <nav class="navbar navbar-expand-lg nav">
@@ -20,12 +20,12 @@
       </div>
       <div class="collapse navbar-collapse">
         <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link text-white" href="?action=home">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-white" href="?action=post">Post</a>
-            </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="?action=home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="?action=post">Post</a>
+          </li>
         </ul>
       </div>
     </nav>
