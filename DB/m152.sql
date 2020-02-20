d16c73901305ab73b79dd182a40b9b9414287771
bluetsuki <kelly.ngn@eduge.ch>
2020-02-20 10:22:48 +0100

    change DB

--- DB/m152.sql
+++ DB/m152.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le :  jeu. 06 fév. 2020 à 11:36
--- Version du serveur :  5.7.17
--- Version de PHP :  7.1.3
+-- Hôte : localhost
+-- Généré le :  jeu. 20 fév. 2020 à 10:21
+-- Version du serveur :  10.3.18-MariaDB-0+deb10u1
+-- Version de PHP :  7.3.11-1~deb10u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,13 +31,13 @@ USE `m152`;
 --
 
 CREATE TABLE `media` (
-  `idMedia` int(11) NOT NULL,
+  `idMedia` int(11) UNSIGNED NOT NULL,
   `typeMedia` varchar(50) NOT NULL,
   `nameMedia` varchar(200) NOT NULL,
-  `dateCreation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `dateModification` timestamp NULL DEFAULT NULL,
+  `dateCreation` date NOT NULL,
+  `dateModification` date DEFAULT NULL,
   `pathImg` varchar(100) NOT NULL,
-  `idPost` int(11) NOT NULL
+  `idPost` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -45,8 +45,7 @@ CREATE TABLE `media` (
 --
 
 INSERT INTO `media` (`idMedia`, `typeMedia`, `nameMedia`, `dateCreation`, `dateModification`, `pathImg`, `idPost`) VALUES
-(26, 'image', 'sunset.jpg', '2020-02-06 10:13:17', NULL, 'media/imgUpload/sunset.jpg', 2),
-(27, 'image', 'sunset_ok.jpg', '2020-02-06 10:13:17', NULL, 'media/imgUpload/sunset_ok.jpg', 2);
+(4, 'image', 'raspiLogo.jpg5e4e4550ea414', '2020-02-20', NULL, 'media/imgUpload/raspiLogo.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -55,10 +54,10 @@ INSERT INTO `media` (`idMedia`, `typeMedia`, `nameMedia`, `dateCreation`, `dateM
 --
 
 CREATE TABLE `post` (
-  `idPost` int(11) NOT NULL,
-  `commentaire` varchar(250) DEFAULT NULL,
-  `dateCreation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `dateModification` timestamp NULL DEFAULT NULL
+  `idPost` int(11) UNSIGNED NOT NULL,
+  `commentaire` varchar(250) NOT NULL,
+  `dateCreation` date NOT NULL,
+  `dateModification` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -66,8 +65,9 @@ CREATE TABLE `post` (
 --
 
 INSERT INTO `post` (`idPost`, `commentaire`, `dateCreation`, `dateModification`) VALUES
-(1, 'asdsadasdasadas', '2020-02-06 09:33:26', NULL),
-(2, NULL, '2020-02-06 10:12:34', NULL);
+(1, 'asds', '2020-02-20', NULL),
+(2, 'asds', '2020-02-20', NULL),
+(3, 'CA MARCHE ?!', '2020-02-20', NULL);
 
 --
 -- Index pour les tables déchargées
@@ -84,8 +84,7 @@ ALTER TABLE `media`
 -- Index pour la table `post`
 --
 ALTER TABLE `post`
-  ADD PRIMARY KEY (`idPost`),
-  ADD UNIQUE KEY `commentaire` (`commentaire`,`dateCreation`);
+  ADD PRIMARY KEY (`idPost`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -95,13 +94,23 @@ ALTER TABLE `post`
 -- AUTO_INCREMENT pour la table `media`
 --
 ALTER TABLE `media`
-  MODIFY `idMedia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `idMedia` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `post`
 --
 ALTER TABLE `post`
-  MODIFY `idPost` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idPost` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `media`
+--
+ALTER TABLE `media`
+  ADD CONSTRAINT `media_ibfk_1` FOREIGN KEY (`idPost`) REFERENCES `post` (`idPost`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
