cb6009a8bfc8ccf29b194ce63117384f2e420936
bluetsuki <kelly.ngn@eduge.ch>
2020-02-20 08:18:42 +0100

    update

--- DB/m152.sql
+++ DB/m152.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 29, 2020 at 10:09 PM
--- Server version: 5.7.17
--- PHP Version: 7.1.3
+-- Hôte : 127.0.0.1
+-- Généré le :  jeu. 06 fév. 2020 à 11:36
+-- Version du serveur :  5.7.17
+-- Version de PHP :  7.1.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `m152`
+-- Base de données :  `m152`
 --
 CREATE DATABASE IF NOT EXISTS `m152` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `m152`;
@@ -27,72 +27,82 @@ USE `m152`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `media`
+-- Structure de la table `media`
 --
 
 CREATE TABLE `media` (
   `idMedia` int(11) NOT NULL,
   `typeMedia` varchar(50) NOT NULL,
   `nameMedia` varchar(200) NOT NULL,
-  `dateCreation` date NOT NULL,
-  `dateModification` date DEFAULT NULL,
-  `pathImg` varchar(100) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `dateCreation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `dateModification` timestamp NULL DEFAULT NULL,
+  `pathImg` varchar(100) NOT NULL,
+  `idPost` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `media`
+-- Déchargement des données de la table `media`
 --
 
-INSERT INTO `media` (`idMedia`, `typeMedia`, `nameMedia`, `dateCreation`, `dateModification`, `pathImg`) VALUES
-(1, 'image', 'ARLAffiche.png', '2020-01-29', NULL, 'upload/ARLAffiche.png'),
-(2, 'image', 'tumblr_ovcll4Wh0l1wvncv4o1_500.jpg', '2020-01-29', NULL, 'upload/tumblr_ovcll4Wh0l1wvncv4o1_500.jpg'),
-(3, 'image', 'ex07.png', '2020-01-29', NULL, 'upload/ex07.png');
+INSERT INTO `media` (`idMedia`, `typeMedia`, `nameMedia`, `dateCreation`, `dateModification`, `pathImg`, `idPost`) VALUES
+(26, 'image', 'sunset.jpg', '2020-02-06 10:13:17', NULL, 'media/imgUpload/sunset.jpg', 2),
+(27, 'image', 'sunset_ok.jpg', '2020-02-06 10:13:17', NULL, 'media/imgUpload/sunset_ok.jpg', 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `post`
+-- Structure de la table `post`
 --
 
 CREATE TABLE `post` (
   `idPost` int(11) NOT NULL,
-  `commentaire` varchar(250) NOT NULL,
-  `dateCreation` date NOT NULL,
-  `dateModification` date DEFAULT NULL,
-  `idMedia` int(11) NOT NULL
+  `commentaire` varchar(250) DEFAULT NULL,
+  `dateCreation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `dateModification` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Indexes for dumped tables
+-- Déchargement des données de la table `post`
+--
+
+INSERT INTO `post` (`idPost`, `commentaire`, `dateCreation`, `dateModification`) VALUES
+(1, 'asdsadasdasadas', '2020-02-06 09:33:26', NULL),
+(2, NULL, '2020-02-06 10:12:34', NULL);
+
+--
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `media`
+-- Index pour la table `media`
 --
 ALTER TABLE `media`
-  ADD PRIMARY KEY (`idMedia`);
+  ADD PRIMARY KEY (`idMedia`),
+  ADD KEY `idPost` (`idPost`);
 
 --
--- Indexes for table `post`
+-- Index pour la table `post`
 --
 ALTER TABLE `post`
   ADD PRIMARY KEY (`idPost`),
-  ADD KEY `idMedia` (`idMedia`);
+  ADD UNIQUE KEY `commentaire` (`commentaire`,`dateCreation`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `media`
+-- AUTO_INCREMENT pour la table `media`
 --
 ALTER TABLE `media`
-  MODIFY `idMedia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idMedia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
 --
--- AUTO_INCREMENT for table `post`
+-- AUTO_INCREMENT pour la table `post`
 --
 ALTER TABLE `post`
-  MODIFY `idPost` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+  MODIFY `idPost` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- controller/post.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once 'model/crud.php';
-require_once "view/post.php";
-require_once "model/post.php";
